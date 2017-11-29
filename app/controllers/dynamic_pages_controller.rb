1a7394b3f2de6d437590e8ee41411111f759cd10
tiagoruzzon <tiagoruzzon@hotmail.com>
2017-11-29 13:06:04 -0200

    alteracoes pesquisa

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -1,5 +1,9 @@
 class DynamicPagesController < ApplicationController
+
+  add_breadcrumb "Página inicial", :root_path
+
   def search
+      add_breadcrumb "Resultados da Pesquisa"
     if !(params[:search].eql? "")
       @dynamic_pages = PgSearch.multisearch(params[:search]).paginate(page: params[:page])
     else
