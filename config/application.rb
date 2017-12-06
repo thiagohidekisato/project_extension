59a7f2fcf388dbb7438fc689fc27fb3737dbddde
TSato <thiagosato22@hotmail.com>
2017-12-06 19:22:25 -0200

    Traduçao

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,10 @@ module ProjectExtension
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-config.i18n.default_locale = :"pt-BR"
-I18n.enforce_available_locales = false
+    config.i18n.default_locale = :"pt-BR"
+    config.time_zone = "Brasilia"
 
-config.time_zone = "Brasilia"
-
-config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
